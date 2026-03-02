de9944524085c7c8741deb88fc09848aba1bf00f
Ortizcarmen <carmen_c19@umes.edu.gt>
2026-03-02 03:34:49 -0600

    feat: add project files from assignment-03

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import './style.css';
+
+document.querySelector('#app').innerHTML = `
+  <h1>Assignment 02 — CDN App</h1>
+  <p>Ortizcarmen — carmen_c19@umes.edu.gt</p>
+  <button id="btn">Probar App</button>
+  <p id="msg"></p>
+`;
+
+document.getElementById('btn').onclick = () => {
+  document.getElementById('msg').innerText =
+    'Aplicación estática lista para CDN 🚀';
+};
